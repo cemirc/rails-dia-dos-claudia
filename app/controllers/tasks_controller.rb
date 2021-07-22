0e954e29f3cd55cbd2edc03afa2558dfeaf55276
claudia miranda <c.mirandacumian@gmail.com>
2021-07-22 15:34:33 -0400

    ejercicio2task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :set_find_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  # New task get
+  def new
+    @record = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to task_path(@task.id)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(params_task)
+    redirect_to task_path(@task.id)
+  end
+
+  def show; end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_find_task
+    @task = Task.find(params[:id])
+  end
+end
